3ae835f065aa157c8da6c37145bc6331c4b2d821
Jade Cutler <jc2681@bath.ac.uk>
2018-11-17 18:30:38 +0000

    Got Tkinter

--- charting.py
+++ charting.py
@@ -1,14 +1,38 @@
+import matplotlib
+
 import backend
 import matplotlib.pyplot as plt
+import tkinter;
+matplotlib.use('TkAgg')
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+from tkinter import *
 
+master = Tk()
+master.title("Engineers Gate Hire Us")
+#-------------------------------------------------------------------------------
 instrumentID = 2
 timeframe = 500
 instrementData = backend.getMarketData(instrumentID, timeframe)
 print(instrementData["name"])
+#
+# plt.plot(list(range((instrementData["currentEpoch"]) + 1 - len(instrementData["data"]), instrementData["currentEpoch"] + 1)), instrementData["data"])
+#
+# plt.show()
+f = Figure(figsize=(5,4), dpi=100)
+a = f.add_subplot(111)
+t = arange(0.0,3.0,0.01)
+s = sin(2*pi*t)
+a.plot(list(range((instrementData["currentEpoch"]) + 1 - len(instrementData["data"]), instrementData["currentEpoch"] + 1)), instrementData["data"])
 
-plt.plot(list(range((instrementData["currentEpoch"]) + 1 - len(instrementData["data"]), instrementData["currentEpoch"] + 1)), instrementData["data"])
+# a.title(instrementData["name"])
+# a.xlabel("epoch")
+# a.ylabel("price")
 
-plt.title(instrementData["name"])
-plt.xlabel("epoch")
-plt.ylabel("price")
-plt.show()
+dataPlot = FigureCanvasTkAgg(f, master=master)
+dataPlot.draw()
+dataPlot.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+#-------------------------------------------------------------------------------
+master.mainloop()
+
